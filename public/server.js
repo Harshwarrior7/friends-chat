dfbd5583afa4e06b90910a0ff06d2bb3c46a3321
Harshwarrior7 <harshwarrior71@gmail.com>
2026-01-13 23:42:51 +0530

    Update server.js

--- public/server.js
+++ public/server.js
@@ -4,21 +4,42 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// This tells the server to serve all files (index.html, CSS, JS) 
-// from the same folder where server.js is located.
+// Serve files from the current directory
 app.use(express.static(__dirname));
 
-// Route for the homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Your socket code
+// Store messages in memory (temporary - will clear on server restart)
+let messages = [];
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    // 1. Handle sending a new message
+    socket.on('chat message', (data) => {
+        // Create a message object with a unique ID
+        const newMessage = {
+            id: Date.now() + Math.random().toString(36).substr(2, 9),
+            user: data.user,
+            text: data.text,
+            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+        };
+        
+        messages.push(newMessage);
+        io.emit('chat message', newMessage);
+    });
+
+    // 2. Handle editing a message
+    socket.on('edit message', (data) => {
+        // Find the message by ID and update its text
+        const msgIndex = messages.findIndex(m => m.id === data.id);
+        if (msgIndex !== -1) {
+            messages[msgIndex].text = data.newText;
+            messages[msgIndex].edited = true;
+            io.emit('message edited', messages[msgIndex]);
+        }
     });
 
     socket.on('disconnect', () => {
@@ -26,7 +47,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Start server using the port Render provides, or 3000 locally
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

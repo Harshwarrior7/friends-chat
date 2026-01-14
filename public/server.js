34d91f620e30aad2eda22d4af9f9aaa81e77fff5
Harshwarrior7 <harshwarrior71@gmail.com>
2026-01-14 12:18:15 +0530

    Update server.js

--- public/server.js
+++ public/server.js
@@ -10,45 +10,15 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-let messages = [];
-
 io.on('connection', (socket) => {
-    // When a user logs in
     socket.on('user joined', (username) => {
-        socket.broadcast.emit('user status', `${username} joined the room`);
+        socket.broadcast.emit('user status', `${username} joined the chat`);
     });
 
     socket.on('chat message', (data) => {
-        const msgObject = { 
-            id: Date.now() + Math.random().toString(36).substr(2, 5), 
-            user: data.user, 
-            text: data.text,
-            replyTo: data.replyTo 
-        };
-        messages.push(msgObject);
-        io.emit('chat message', msgObject);
-    });
-
-    socket.on('typing', (user) => socket.broadcast.emit('display typing', user));
-    socket.on('stop typing', () => socket.broadcast.emit('hide typing'));
-
-    socket.on('edit message', (data) => {
-        const msg = messages.find(m => m.id === data.id);
-        if (msg) {
-            msg.text = data.newText;
-            io.emit('message edited', { id: data.id, text: data.newText });
-        }
-    });
-
-    socket.on('delete message', (id) => {
-        messages = messages.filter(m => m.id !== id);
-        io.emit('message deleted', id);
-    });
-
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('hide typing');
+        io.emit('chat message', data);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server live on port ${PORT}`));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
